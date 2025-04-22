fc0f91c15b4f7b7e799c3f15779ed4c673b7795d
khaerunniszaa <deakhaerunniszaa@gmail.com>
2025-04-22 16:01:01 +0700

    Fitur Landing Page

--- routes/web.php
+++ routes/web.php
@@ -8,22 +8,9 @@ Route::get('/', function () {
     return view('pages.landing');
 });
 
-Route::get('/dashboard', function () {
-    return view('pages.dashboard');
-});
 
 // Authentication Routes
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
-Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-// Profile Routes
-Route::middleware(['auth'])->group(function () {
-    Route::get('/profile/personal', [ProfileController::class, 'personal'])->name('pages.profile.personal');
-    Route::get('/profile/analys', [ProfileController::class, 'analys'])->name('pages.profile.analys');
-    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('pages.profile.editData');
-    Route::put('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
-});
 
