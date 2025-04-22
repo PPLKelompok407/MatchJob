1d721d385bf6c9363f7a6126ea547bffcbfbfc98
khily <khilyahdanyoga@gmail.com>
2025-04-22 18:08:35 +0700

    Unit Test Profile

--- tests/Feature/PersonalTest.php
+++ tests/Feature/PersonalTest.php
@@ -7,22 +7,24 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
 
-class CobaTest extends TestCase
+class PersonalTest extends TestCase
 {
     use RefreshDatabase;
+
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+    public function test_profile(): void
     {
         // Buat user testing
         $user = User::factory()->create();
-
+        
         // Login sebagai user testing
         $this->actingAs($user);
 
+        // Akses endpoint yang dilindungi oleh middleware auth
         $response = $this->get('/profile/personal');
-
+        
         $response->assertStatus(200);
     }
 }
